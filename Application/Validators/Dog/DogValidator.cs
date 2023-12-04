de81d3cfa20cd8258aff54b6ebb837a882c36828
felixhard <felixhard32@gmail.com>
2023-12-04 15:19:20 +0100

    Påbörjat validations

---
+++ Application/Validators/Dog/DogValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.AnimalDtos.DogDto;
+using FluentValidation;
+
+namespace Application.Validators.Dog
+{
+    public class DogValidator : AbstractValidator<DogDto>
+    {
+        public DogValidator()
+        {
+            RuleFor(dog => dog.Name).NotEmpty()
+                    .WithMessage("Dog name cannot be empty B.F.A.M")
+                    .NotNull().WithMessage("Dog name cannot be NULL");
+
+        }
+
+    }
+}
---
+++ Application/Validators/GuidValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+
+namespace Application.Validators
+{
+    public class GuidValidator : AbstractValidator<Guid>
+    {
+        public GuidValidator()
+        {
+            RuleFor(guid => guid)
+                .NotEmpty().WithMessage("Guid should not be empty")
+                .NotEqual(Guid.Empty).WithMessage("Guid cannot be empty Guid");
+        }
+    }
+}
